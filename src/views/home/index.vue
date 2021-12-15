0df3eef27adc61191fe5bdeb18d621aa1d9d159f
bysch <1259364843@qq.com>
2021-12-15 21:02:28 +0800

    首页添加随机语录

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,22 +1,34 @@
 <!--  -->
 <template>
-   <div class='home'>
-       <h1>home首页</h1>
-   </div>
+  <div class="home">
+    <el-row>
+      <el-col :span="24"
+        ><div class="grid-content bg-purple-dark">
+          <div class="dailywords">
+            <div class="dailywords-words">
+              <p>{{ sentence.content }}</p>
+            </div>
+            <div class="dailywords-origin">
+              <span class="tag">《{{ sentence.tag }}》</span>
+              <span>————{{ sentence.origin }}</span>
+            </div>
+          </div>
+        </div></el-col
+      >
+    </el-row>
+  </div>
 </template>
 
 <script>
-// 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
-// 例如：import 《组件名称》 from '《组件路径》';
-
+import axios from 'axios'
 export default {
   // import引入的组件需要注入到对象中才能使用
   name: 'HomeIndex',
   components: {},
-  data () {
+  data() {
     // 这里存放数据
     return {
-
+      sentence: {}
     }
   },
   // 监听属性 类似于data概念
@@ -24,25 +36,46 @@ export default {
   // 监控data中的数据变化
   watch: {},
   // 方法集合
-  methods: {
-
-  },
+  methods: {},
   // 生命周期 - 创建完成（可以访问当前this实例）
-  created () {
-
-  },
+  created() {},
   // 生命周期 - 挂载完成（可以访问DOM元素）
-  mounted () {
-
+  mounted() {
+    axios({
+      method: 'GET',
+      url: 'https://api.xygeng.cn/one'
+    }).then((res) => {
+      const { data } = res.data
+      this.sentence = data
+      console.log(data)
+    })
   },
-  beforeCreate () {}, // 生命周期 - 创建之前
-  beforeMount () {}, // 生命周期 - 挂载之前
-  beforeUpdate () {}, // 生命周期 - 更新之前
-  updated () {}, // 生命周期 - 更新之后
-  beforeDestroy () {}, // 生命周期 - 销毁之前
-  destroyed () {}, // 生命周期 - 销毁完成
-  activated () {} // 如果页面有keep-alive缓存功能，这个函数会触发
-  }
+  beforeCreate() {}, // 生命周期 - 创建之前
+  beforeMount() {}, // 生命周期 - 挂载之前
+  beforeUpdate() {}, // 生命周期 - 更新之前
+  updated() {}, // 生命周期 - 更新之后
+  beforeDestroy() {}, // 生命周期 - 销毁之前
+  destroyed() {}, // 生命周期 - 销毁完成
+  activated() {} // 如果页面有keep-alive缓存功能，这个函数会触发
+}
 </script>
-<style lang='less' scoped>
+<style lang="less" scoped>
+.dailywords {
+  width: 70%;
+  height: 100px;
+  margin: 40px auto 0px auto;
+  .dailywords-words {
+    width: 100%;
+    font-size: 25px;
+  }
+  .dailywords-origin {
+    position: relative;
+    text-align: right;
+    font-size: 20px;
+    .tag {
+      position: absolute;
+      left: 0;
+    }
+  }
+}
 </style>
